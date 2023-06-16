68ab1ef0ab44aff858d0db74d6eed41c4a625849
杨怡 <639293382@qq.com>
2023-06-16 17:48:08 +0800

    new

--- vite.config.ts
+++ vite.config.ts
@@ -170,8 +170,8 @@ export default defineConfig({
     //  host: 'https://test.xxchat.chat/',
     proxy: {
       '/api': {
-        target: 'https://test.xxchat.chat/',
-        // target: 'http://8.134.216.253:8000/',
+        target: 'https://test.xxchat.chat/api',
+        // target: 'http://8.134.216.253:8000/api',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
