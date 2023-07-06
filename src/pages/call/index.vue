13ef00672a1695584c6259f9edc2d8ca34e55209
fugui12321 <1183927233@qq.com>
2023-07-06 12:19:56 +0800

    new

--- src/pages/call/index.vue
+++ src/pages/call/index.vue
@@ -2,9 +2,10 @@
     <div>
         <div class="relative  max-w-450  relative  overflow-hidden" ref="remoteVideoContent"
             :style="{ height: viewHeight + 'px' }">
-            <!-- <video ref="videoPlay" class="w-full h-full object-cover" :src="robotVideoList" autoplay="autoplay"
-                muted="muted" v-if="fromMatch || pushRobot"></video> -->
-            <video :src="robotVideoList" autoplay playsinline muted controls class="w100vh object-cover"></video>
+            <video ref="videoPlay" class="w-full h-full object-cover"
+                src="https://jdvodlzmcwbkr.vod.126.net/jdvodlzmcwbkr/65-1344695788849105-1647831230565-0.mp4" autoplay muted
+                v-if="fromMatch || pushRobot"></video>
+            <!-- <video :src="robotVideoList" autoplay playsinline muted controls class="w100vh object-cover"></video> -->
             <!-- 本地视频窗口 -->
             <img :src="userStore.userDetail.icon" class="absolute w-full h-full z--1 blur-10">
             <div ref="localVideoContent"
